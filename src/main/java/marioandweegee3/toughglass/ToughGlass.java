099fa9c06a0add6dfc3e2cb229eb076b946c36a1
MarioAndWeegee3 <marioandweegee3@gmail.com>
2023-06-15 13:31:20 -0400

    # Version 3.0.0
    * Ported to 1.20.1
    * Added Tough Glass Pane; has the same properties as the full block, but with the small size of normal glass panes.
    * Changed the color palette used by the glass to look closer to Obsidian.
    * Changed the logo to reflect changes to the block design
      * The logo now uses the [Cozette](https://github.com/slavfox/Cozette) font
    * Made Tough Glass (Panes) change the Note Block instrument to Hat when placed below.

--- src/main/java/marioandweegee3/toughglass/ToughGlass.java
+++ src/main/java/marioandweegee3/toughglass/ToughGlass.java
@@ -1,46 +1,74 @@
 package marioandweegee3.toughglass;
 
 import marioandweegee3.toughglass.block.ToughGlassBlock;
+import marioandweegee3.toughglass.block.ToughGlassPaneBlock;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricMaterialBuilder;
 import net.minecraft.block.MapColor;
-import net.minecraft.block.Material;
+import net.minecraft.block.enums.Instrument;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public final class ToughGlass implements ModInitializer {
     public static final String modId = "toughglass";
 
-    public static final Material toughGlassMaterial =
-        new FabricMaterialBuilder(MapColor.CLEAR)
-            .blocksPistons()
-            .notSolid()
-            .build();
-
     public static final ToughGlassBlock toughGlassBlock = new ToughGlassBlock(
-        FabricBlockSettings.of(toughGlassMaterial)
+        FabricBlockSettings.create()
+            .pistonBehavior(PistonBehavior.BLOCK)
             .hardness(10)
             .resistance(1200)
             .nonOpaque()
+            .mapColor(MapColor.CLEAR)
+            .instrument(Instrument.HAT)
+            .sounds(BlockSoundGroup.GLASS)
+            .requiresTool()
+    );
+
+    public static final ToughGlassPaneBlock toughGlassPaneBlock = new ToughGlassPaneBlock(
+        FabricBlockSettings.copyOf(toughGlassBlock)
+    );
+
+    public static final BlockItem toughGlassItem = new BlockItem(
+        toughGlassBlock,
+        new FabricItemSettings()
+    );
+
+    public static final BlockItem toughGlassPaneItem = new BlockItem(
+        toughGlassPaneBlock,
+        new FabricItemSettings()
     );
 
     @Override
     public void onInitialize() {
         Registry.register(
-            Registry.BLOCK, new Identifier(modId, "tough_glass"),
+            Registries.BLOCK, new Identifier(modId, "tough_glass"),
             toughGlassBlock
         );
         Registry.register(
-            Registry.ITEM, new Identifier(modId, "tough_glass"),
-            new BlockItem(
-                toughGlassBlock,
-                new FabricItemSettings()
-                    .group(ItemGroup.BUILDING_BLOCKS)
-            )
+            Registries.BLOCK, new Identifier(modId, "tough_glass_pane"),
+            toughGlassPaneBlock
         );
+
+        Registry.register(
+            Registries.ITEM, new Identifier(modId, "tough_glass"),
+            toughGlassItem
+        );
+        Registry.register(
+            Registries.ITEM, new Identifier(modId, "tough_glass_pane"),
+            toughGlassPaneItem
+        );
+
+        //noinspection UnstableApiUsage
+        ItemGroupEvents.modifyEntriesEvent(
+            ItemGroups.COLORED_BLOCKS
+        ).register(content -> content.addAfter(Items.GLASS_PANE, toughGlassItem, toughGlassPaneItem));
     }
 }
